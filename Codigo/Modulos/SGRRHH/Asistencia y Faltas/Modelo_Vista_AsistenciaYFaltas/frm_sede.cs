981be787f114678dbc2bc5115622b915049d72fc
Marco Alejandro Monroy Rousselin <mmonroyr3@miumg.edu.gt>
2025-06-05 09:12:26 -0600

    Subida Final

--- Codigo/Modulos/SGRRHH/Asistencia y Faltas/Modelo_Vista_AsistenciaYFaltas/frm_sede.cs
+++ Codigo/Modulos/SGRRHH/Asistencia y Faltas/Modelo_Vista_AsistenciaYFaltas/frm_sede.cs
@@ -23,19 +23,16 @@ namespace Modelo_Vista_AsistenciaYFaltas
             string idUsuario = Interfac_V3.UsuarioSesion.GetIdUsuario();
             /// Marco Alejandro Monroy*/
             /*Prueba con navegador*/
-            string[] alias = { "pk_id_permiso", "fk_id_empleado", "fecha_inicio", "fecha_fin", " tipo_permiso", " aprobado", " estado", "con_goce_sueldo" };
+            string[] alias = { "codigo_sede", " nombre_sede", "estatus_sede", " estado", };
             navegador1.AsignarAlias(alias);
             navegador1.AsignarSalida(this);
             navegador1.AsignarColorFondo(ColorTranslator.FromHtml("#B4D2F0"));
             navegador1.AsignarColorFuente(Color.Black);
-            navegador1.AsignarTabla("tbl_permisos");
+            navegador1.AsignarTabla("sedes");
             navegador1.ObtenerIdAplicacion("16002");
             navegador1.ObtenerIdUsuario(idUsuario);
             navegador1.AsignarAyuda("1");
-            navegador1.AsignarNombreForm("Asignacion de permisos");
-
-
-            navegador1.AsignarComboConTabla("tbl_empleados", "pk_clave", "empleados_nombre", 1);
+            navegador1.AsignarNombreForm("Asignacion de Sedes");
         }
     }
 }
